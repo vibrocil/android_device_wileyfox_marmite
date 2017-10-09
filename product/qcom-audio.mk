c6446a05c877e7e61a22f4dc61fcccaf54329579
vibrocil <michiel.verstraete.mv@gmail.com>
2017-10-09 18:56:06 +0200

    marmite: adjust audio for build
    Change-Id: I924dae40536bcdb61b7d94c317f6fd896eff2676

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -32,7 +32,6 @@ PRODUCT_COPY_FILES += \
 # Based on (https://github.com/LineageOS/android/commit/64e1481a189d34c2e9fa603571f1158164e3654d)
 PRODUCT_COPY_FILES += \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    hardware/qcom/audio-caf/msm8996/configs/msm8937/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml \
