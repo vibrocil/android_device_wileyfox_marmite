466f24c16b389abe966ece66f2a0b704e22eef5a
vibrocil <michiel.verstraete.mv@gmail.com>
2017-10-24 12:52:49 +0200

    Initial
    Change-Id: I553c1c21c6e89adc7adc66b8498f9f3a8ab21d0e

---
+++ board/twrp.mk
@@ -0,0 +1,8 @@
+TARGET_RECOVERY_FSTAB := $(BOARD_PATH)/rootdir/root/fstab.qcom
+TW_THEME := portrait_hdpi
+BOARD_HAS_NO_REAL_SDCARD := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_DEFAULT_LANGUAGE := en-US
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_CRYPTO := true
